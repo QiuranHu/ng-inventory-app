cd1547b6ac899ffa1aedf33be05270f1c6523ff0
Qiuran Hu <qiuranh@gmail.com>
2021-08-12 08:25:08 +0800

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'inventory-app-root',
-  template: ` <div class="inventory-app">(Products will go here soon)</div>`,
+  template: `<div class="inventory-app">
+    <!-- $event is a special variable here that represents the thing sent to the output. -->
+    <products-list
+      [productList]="products"
+      (onProductSelected)="productWasSelected($event)"
+    ></products-list>
+  </div>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  products: Product[];
   title = 'inventory-app';
+
+  constructor() {
+    this.products = [
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessoried', 'Hats'],
+        29.9
+      ),
+      new Product(
+        'NICEHAT2',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessoried', 'Hats'],
+        29.9
+      ),
+      new Product(
+        'NICEHAT3',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessoried', 'Hats'],
+        29.9
+      ),
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
+  }
 }
---
+++ src/app/product-row/product-row.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Product } from '../product.model';
+
+/**
+ * @ProductRow: A component for the view of single Product.
+ * */
+@Component({
+  selector: 'product-row',
+  templateUrl: './product-row.component.html',
+  styleUrls: ['./product-row.component.css'],
+})
+export class ProductRowComponent implements OnInit {
+  @Input() product!: Product;
+  @HostBinding('attr.class') cssClass = 'item';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
